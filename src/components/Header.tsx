babf02d6bdfa949667aec8a021ea1d35ff0dc702
Dario Alvarez <daralv53@gmail.com>
2024-10-07 17:49:56 -0600

    creando header

---
+++ src/components/Header.tsx
@@ -0,0 +1,21 @@
+
+
+export default function Header() {
+  return (
+    <header className="bg-slate-800">
+        <div className="mx-auto container px-5 py-16">
+            <div className="flex justify-between items-center">
+                {/* Logotipo */}
+                <div>
+
+                </div>
+
+                {/* Navegación */}
+                <nav>
+                    
+                </nav>
+            </div>
+        </div>
+    </header>
+  )
+}
