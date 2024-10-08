c55a61f436e66e3e245fddb4dc840e9689a3a883
Dario Alvarez <daralv53@gmail.com>
2024-10-08 13:45:35 -0600

    Conectando multiples stores

--- src/stores/recipeSlice.ts
+++ src/stores/recipeSlice.ts
@@ -1,5 +1,12 @@
+import { StateCreator } from "zustand"
+
+type Category = {}
+
+export type RecipeSliceType = {
+    categories: Category[]
+}
 
 
-export const createRecipesSlice = () => ({
+export const createRecipesSlice: StateCreator<RecipeSliceType> = () => ({ //StateCreator nos va a permitir crear el state
     categories: []
 })
