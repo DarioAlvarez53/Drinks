2a7f2a31f0a3df0091b504e53f7fc27c3a33eb31
Dario Alvarez <daralv53@gmail.com>
2025-01-07 13:21:53 -0600

    Primer parte mostrando bebeidas

--- src/pages/IndexPage.tsx
+++ src/pages/IndexPage.tsx
@@ -1,7 +1,24 @@
+import { useMemo } from "react"
+import { useAppStore } from "../stores/useAppStore"
+
 export default function IndexPage() {
+  const drinks = useAppStore((state) => state.drinks)
+
+  const hasDrinks = useMemo(() =>drinks.drinks.length, [drinks])
+
   return (
     <>
-      <h1>Index</h1>
+      <h1 className="text-6xl font-extrabold">Recetas</h1>
+
+      {hasDrinks ? (
+        <>
+          <p>Si hay bebidas</p>
+        </>
+      ) : (
+        <p className="my-10 text-center text-2xl">
+          No hay resultados aun, utiliza el formulario para buscar recetas
+        </p>
+      )}
     </>
   )
 }
